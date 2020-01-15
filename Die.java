b99d77334b51267df5b6010c91ae9b84b066255a
Beatrice van der Velde - Kraft <beavdvk@gmail.com>
2020-01-15 08:53:01 -0800

    die class completegit add Die.java

--- Die.java
+++ Die.java
@@ -2,13 +2,13 @@
 /**
  * Write a description of class Die here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Beatrice
+ * @version 2020 - 1 - 15
  */
 public class Die
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int result;
 
     /**
      * Constructor for objects of class Die
@@ -16,18 +16,25 @@ public class Die
     public Die()
     {
         // initialise instance variables
-        x = 0;
+        result = roll();
     }
-
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * calculate the roll
+     * 
      */
-    public int sampleMethod(int y)
+    public int roll()
     {
         // put your code here
-        return x + y;
+        result = (int) (Math.random() * 6 + 1);
+        return result; //have it both places
     }
+    /**
+     * returns the reslut of the roll
+     * @return    the result
+     */
+    public int getResult()
+    {
+        return result;
+    }
+  
 }
